a5dcee47c491e9d482fc3621b63413c8ad959472
Hector Gomez <hgomezm92@gmail.com>
2025-12-09 22:49:22 +0100

    Add fireball system and test enemy

--- scenes/fireball.gd
+++ scenes/fireball.gd
@@ -3,18 +3,23 @@ extends Area2D
 @export var animation: AnimatedSprite2D
 @export var speed: float = 250.0
 var direction: Vector2 = Vector2.ZERO
+var _is_exploding: bool
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if direction != Vector2.ZERO:
+	if direction != Vector2.ZERO and not _is_exploding:
 		position += direction * speed * delta
-		animation.play("default")
-	else:
-		animation.stop()
+		animation.play("move")
+	
+	if animation.is_playing() == false:
+		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
+	_explode()
 
+func _explode():
+	_is_exploding = true
+	animation.play("explode")
 
 func _on_body_entered(body: Node2D) -> void:
-	hide()
+	_explode()
